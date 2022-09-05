7e3c60ae5cbe8a5d5fb65b3c82ac1a42df971ba6
Phil Scott <phil.scott@gmail.com>
2022-09-04 21:40:35 -0400

    reworking everything into methods

--- src/IconSourceGen/Generator.cs
+++ src/IconSourceGen/Generator.cs
@@ -17,32 +17,32 @@ public class Generator : IIncrementalGenerator
         var namesAndContents = context.AdditionalTextsProvider
             .Combine(config)
             .Where(static value => value.Left.Path.EndsWith(".svg"))
-            .Select(static (value, token) => (Config: value.Right, Path: value.Left.Path, Content: value.Left.GetText(token)!.ToString()))
+            .Select(static (value, token) => (Config: value.Right, Path: value.Left.Path,
+                Content: value.Left.GetText(token)!.ToString()))
             .Select(static (value, token) => ParseSvg(value))
             .Where(i => i != null);
 
+        context.RegisterSourceOutput(config, AddDefaultClasses);
         context.RegisterSourceOutput(namesAndContents, Execute);
     }
 
-    private static void Execute(SourceProductionContext spc, (string ns, string className, string viewBox, string content)? value)
+    private void AddDefaultClasses(SourceProductionContext context, Config config)
     {
-        if (value == null) return;
-
-        var (ns, className, viewBox, content) = value.Value;
-
-        var source = $$$"""
+        var src = $$""" 
+#nullable enable
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.CompilerServices;
 using Microsoft.AspNetCore.Components.Rendering;
 
-#nullable enable
-namespace {{{ns}}}
+namespace {{ config.RootNameSpace}} 
 {
-    public class {{{className}}} : ComponentBase
+    public class Icon : ComponentBase
     {
         [Parameter(CaptureUnmatchedValues = true)]
         public Dictionary<string, object>? AdditionalAttributes { get; set; }
 
+        [Parameter, EditorRequired]
+        public IconData? Data { get; set; } = null!;
+    
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
             string? cssClass = null;
@@ -51,28 +51,57 @@ namespace {{{ns}}}
                 cssClass = classObject.ToString();
             }
 
-#nullable disable
-
+            #nullable disable
             builder.OpenElement(0, "svg");
             builder.AddAttribute(1, "xmlns", "http://www.w3.org/2000/svg");
             builder.AddAttribute(2, "class", cssClass);
-            const string ViewBox = "{{{viewBox}}}";
-            const string Content = "{{{content.Replace("\"","\\\"")}}}";
-            builder.AddAttribute(3, "viewBox", ViewBox);
-            builder.AddMultipleAttributes(4, this.AdditionalAttributes?.Where(i => i.Key != "class"));
-            builder.AddMarkupContent(5, Content);
+            var viewBox = Data.ViewBox;
+            var content = Data.Content;
+            builder.AddAttribute(3, "viewBox", viewBox);
+            builder.AddMultipleAttributes(4, AdditionalAttributes?.Where(i => i.Key != "class" && i.Value is not IconData) );
+            builder.AddMarkupContent(5, content);
             builder.CloseElement();
         }
     }
+
+    public class IconData
+    {
+        public string Content { get; init; }
+        public string ViewBox { get; init; }
+    }
 }
-""";
+""" ;
 
-        spc.AddSource($"{ns}-{className}.g.cs", source);
+        context.AddSource("component-and-data.g.cs", src);
+    }
 
+    private static void Execute(SourceProductionContext spc,
+        (string ns, string className, string iconName, string viewBox, string content)? value)
+    {
+        if (value == null) return;
+
+        var (ns, className, iconName, viewBox, content) = value.Value;
+
+        var source = $$$"""
+namespace {{{ ns}}}  
+{
+    public static partial class {{{ className}}} 
+    {
+        public static IconData {{{ iconName}}} () => new()
+        {
+            Content = "{{{ content.Replace("\"", "\\\"")}}}",
+            ViewBox = "{{{ viewBox}}} "
+        };
+    }
+}
+""" ;
+
+        spc.AddSource($"{ns}-{className}-{iconName}.g.cs", source);
     }
 
 
-    private static (string ns, string className, string viewBox, string content)? ParseSvg((Config Config, string Path, string Content) value)
+    private static (string ns, string className, string iconName, string viewBox, string content)? ParseSvg(
+        (Config Config, string Path, string Content) value)
     {
         var path = Path.GetDirectoryName(value.Path);
         if (path == null)
@@ -101,7 +130,7 @@ namespace {{{ns}}}
         var fileName = Path.GetFileNameWithoutExtension(absolutePath);
         var doc = new XmlDocument();
         doc.LoadXml(value.Content);
-        var node  = doc.FirstChild;
+        var node = doc.FirstChild;
         if (node.Attributes == null)
         {
             return null;
@@ -110,12 +139,12 @@ namespace {{{ns}}}
         var viewBox = node.Attributes["viewBox"].Value;
         // ok, this is beyond naive but whatever.
         var content = node.InnerXml;
-        var ns = $"{value.Config.RootNameSpace}.{path.Replace("/", ".").Replace("\\", ".")}";
-        ns = string.Join(".", ns.Split('.').Select(i => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(i)).ToArray());
-        var className = GenerateClassName(fileName);
+        var className = path.Replace("/", ".").Replace("\\", ".");
+        className = string.Join("",
+            className.Split('.').Select(i => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(i)).ToArray());
+        var iconName = GenerateIconName(fileName);
 
-        return (ns, className, viewBox, content);
-
+        return (value.Config.RootNameSpace, className, iconName, viewBox, content);
     }
 
     private static IncrementalValueProvider<Config> GetConfigValue(
@@ -123,7 +152,7 @@ namespace {{{ns}}}
     {
         var config = context.AnalyzerConfigOptionsProvider.Select((provider, _) =>
         {
-            var ns = "Icons";
+            var ns = "BadIdeas.FontAwesome.Icons";
             var rootFolder = "svgs";
 
             if (provider.GlobalOptions.TryGetValue("icon_gen_root_namespace", out var configValue))
@@ -141,10 +170,11 @@ namespace {{{ns}}}
         return config;
     }
 
-    private static string GenerateClassName(string value)
+    private static string GenerateIconName(string value)
     {
         var className = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(value);
-        className = Regex.Replace(className, @"[^\p{Ll}\p{Lu}\p{Lt}\p{Lo}\p{Nd}\p{Nl}\p{Mn}\p{Mc}\p{Cf}\p{Pc}\p{Lm}]", "");
+        className = Regex.Replace(className, @"[^\p{Ll}\p{Lu}\p{Lt}\p{Lo}\p{Nd}\p{Nl}\p{Mn}\p{Mc}\p{Cf}\p{Pc}\p{Lm}]",
+            "");
 
         // Class name doesn't begin with a letter, insert an underscore
         if (!char.IsLetter(className, 0))
@@ -153,6 +183,6 @@ namespace {{{ns}}}
         }
 
         className = className.Replace(" ", string.Empty);
-        return $"{className}Icon";
+        return className;
     }
 }
---
+++ tests/TestBlazorApp/Program.cs
@@ -0,0 +1,12 @@
+global using static BadIdeas.FontAwesome.Icons.Brands;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TestBlazorApp;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
